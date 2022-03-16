f83a42ee523a6b619eb267dac6bb31e7c7e8efe8
Jenifer19IFC <jenifergoedert10@gmail.com>
2022-03-15 21:08:32 -0300

    commit

--- src/listasLineares/TesteLista.java
+++ src/listasLineares/TesteLista.java
@@ -19,29 +19,29 @@ public class TesteLista {
 		//Ele mostra primeiro o último que eu inseri para trás
 		System.out.println(L1.mostraLista());
 		
-		//L1.deleteComValor(8);
-		//System.out.println(L1.mostraLista());
+		L1.deleteComValor(8);
+		System.out.println(L1.mostraLista());
 		
-		//System.out.println(L1.contNodos() + " nodos");
-		//L1.inserirFinal(7);
-		//System.out.println(L1.mostraLista());
+		System.out.println(L1.contNodos() + " nodos");
+		L1.inserirFinal(7);
+		System.out.println(L1.mostraLista());
 		
-		//L1.inserirFinal(11);
-		//L1.inserirFinal(34);
-		//System.out.println(L1.mostraLista());
+		L1.inserirFinal(11);
+		L1.inserirFinal(34);
+		System.out.println(L1.mostraLista());
 		
-//		System.out.println("Deletar primeiro:\n");
-//		L1.deletarPrimeiroNodo();
-//		System.out.println(L1.mostraLista());
-//		
-//		L1.deletarPrimeiroNodo();
-//		System.out.println(L1.mostraLista());
-//		
-//		L1.deletarFinal();
-//		System.out.println(L1.mostraLista());
+		System.out.println("Deletar primeiro:\n");
+		L1.deletarPrimeiroNodo();
+		System.out.println(L1.mostraLista());
 		
-//		System.out.println("Contar ");
-//		System.out.println(L1.contPosicao(10));
+		L1.deletarPrimeiroNodo();
+		System.out.println(L1.mostraLista());
+		
+		L1.deletarFinal();
+		System.out.println(L1.mostraLista());
+		
+		System.out.println("Contar ");
+		System.out.println(L1.contPosicao(10));
 		
 		System.out.println(L1.contPosicao(11));
  	}
